16a4e1b2098aa6c84843325528c024f4b6d7d4b6
marsden garner <marsdenjgpersonal@gmail.com>
2025-02-23 09:43:50 +0000

    Fixed Bug with get_crew_count(). Now split into two functions. get_active_crew_count(). get_total_crew_count(). Added a state enum to the Crewmember struct. Three possible states: Active, Deactive, Dead. Added is_sick boolean to the Crewmember struct. Have created a compiled Executable for the main.c. However it does not print anything to the console. Possibly bugged. Will try to fix.

--- src/main.c
+++ src/main.c
@@ -20,12 +20,18 @@
 //
 // Struct for Crewmember 
 
+// Enum State for Crewmember
+
+typedef enum Crew_State { Active = 0, Deactive = 1, Dead = 2 } Crew_State;
+
 typedef struct Crewmember 
 {
   double health;
   double energy;
   double wellbeing;
   int day_without_life_supply;
+  Crew_State state;
+  bool is_sick;
 } Crewmember;
 
 // Struct for Space_Station
@@ -36,7 +42,7 @@ typedef struct Space_Station
   double water_supply;
   double oxygen_supply;
   double energy_supply;
-  Crewmember crew[INITIAL_CREW_MEMBER_NUM];
+  Crewmember* crew[INITIAL_CREW_MEMBER_NUM];
 } Space_Station;
 
 // Initalise Members in the Space Station
@@ -48,22 +54,46 @@ void initialise_space_station(Space_Station* station)
   station->water_supply = INITIAL_WATER_GALLON;
   station->oxygen_supply = INITIAL_OXYGEN;
   station->energy_supply = INITIAL_ENERGY_SUPPLY;
-  
+
   // Initialise variables for the crewmembers of station
   for (int i = 0; i < INITIAL_CREW_MEMBER_NUM; i++)
   {
-    station->crew[i].health = 100;
-    station->crew[i].energy = 100;
-    station->crew[i].wellbeing = 100;
-    station->crew[i].day_without_life_supply = 0;
+    station->crew[i]->health = 100;
+    station->crew[i]->energy = 100;
+    station->crew[i]->wellbeing = 100;
+    station->crew[i]->day_without_life_supply = 0;
+    station->crew[i]->state = Active;
+    station->crew[i]->is_sick = false;
   }
 }
 // Function to get Crew Count
 
-int get_crew_count(Space_Station* station)
+int get_active_crew_count(Space_Station* station)
 {
-  int crew_count = sizeof(station->crew) / sizeof(Crewmember);
-  return crew_count;
+  int active_crew_count;
+  for (int i = 0; i < INITIAL_CREW_MEMBER_NUM; i++)
+  {
+    if (station->crew[i]->state == Active)
+    {
+      active_crew_count++;
+    }
+  }
+
+  return active_crew_count;
+}
+
+int get_total_crew_count(Space_Station* station)
+{
+  int total_crew_count;
+  for (int i = 0; i < INITIAL_CREW_MEMBER_NUM; i++)
+  {
+    if (station->crew[i]->state != Dead)
+    {
+      total_crew_count++;
+    }
+  }
+
+  return total_crew_count;
 }
 
 // Function to simulation one day / iteration of simulation
@@ -71,23 +101,31 @@ int get_crew_count(Space_Station* station)
 void simulate_iteration(Space_Station* station)
 {
   // Change Stations supply counts
-  station->food_supply =- (FOOD_CALORIES_CONSUMPTION_PERSON * CREW_MEMBER_NUM);
-  station->water_supply -= (WATER_GALLON_CONSUMPTION_PERSON * (get_crew_count()));
+  station->food_supply -= (FOOD_CALORIES_CONSUMPTION_PERSON * get_total_crew_count(station));
+  station->water_supply -= (WATER_GALLON_CONSUMPTION_PERSON * get_total_crew_count(station));
   station->oxygen_supply -= 10; // TODO: MAKE OXYGEN SUPPLY VARIABLE ON ACTUAL AMOUNT
-  station->energy_supply -= (INITIAL_ENERGY_SUPPLY * 0.1) // Subtracts energy count by 10% of inital energy count;
+  station->energy_supply -= (INITIAL_ENERGY_SUPPLY * 0.1); // Subtracts energy count by 10% of inital energy count;
   
   // Updates Crewmembers variables
 
-  for (int i = 0; i < (get_crew_count()); i++)
+  for (int i = 0; i < get_total_crew_count(station); i++)
   {
-    station->crew[i].energy -= 8;
-    station->crew[i].wellbeing -= 5;
+    if (station->crew[i]->state == Active)
+    {
+      station->crew[i]->energy -= 8;
+      station->crew[i]->wellbeing -= 5;
+    }
+    else if (station->crew[i]->state == Deactive)
+    {
+      station->crew[i]->energy -= 2;
+      station->crew[i]->wellbeing -= 7;
+    }
   }
 
   // Prints Data to the Console
 
   printf("Station Supplies:\n");
-  printf("Food: %d, Water: %d, Oxygen: %d, Energy: %d\n",
+  printf("Food (CALORIES): %lf, Water (GALLONS): %lf, Oxygen: %lf, Energy (JOULES): %lf\n",
          station->food_supply,
          station->water_supply,
          station->oxygen_supply,
@@ -96,14 +134,25 @@ void simulate_iteration(Space_Station* station)
   printf("Crewmember attributes:\n");
   printf("----------------------\n");
   
-  for (int i = 0; i < (get_crew_count()); i++)
+  for (int i = 0; i < get_total_crew_count(station); i++)
   {
     printf("Crewmember %i:\n", i);
-    printf("Health: %d, Energy: %d, Wellbeing: %d, Days Without Life-Needing Supplies: %d",
-          station->crew[i].health,
-          station->crew[i].energy,
-          station->crew[i].wellbeing,
-          station->crew[i].days_without_life_supply);
-    printf("-_-_-_-_-_-_-_-_-_-_-");
+    printf("Health: %lf, Energy: %lf, Wellbeing: %lf, Days Without Life-Needing Supplies: %i, State: %i, Is Sick: %i\n",
+          station->crew[i]->health,
+          station->crew[i]->energy,
+          station->crew[i]->wellbeing,
+          station->crew[i]->day_without_life_supply),
+          station->crew[i]->state,
+          station->crew[i]->is_sick;
+    printf("-_-_-_-_-_-_-_-_-_-_-\n");
   }
+}
+
+int main()
+{
+  Space_Station space_station;
+  initialise_space_station(&space_station);
+  simulate_iteration(&space_station);
+
+  return 0;
 }
