516200f4f6b4f76985c0ed968c6080041d8e1e7f
Marsden Garner <marsdenjgpersonal@gmail.com>
2025-02-25 05:39:02 +0000

    Fixed Bug in rand_events.c. It was related to assigning an integer to damage_caused variable (a int*). Now fixed and program compiles fine.

--- src/includes/rand_events.c
+++ src/includes/rand_events.c
@@ -15,11 +15,11 @@ void create_dust_storm()
 typedef struct Asteroid
 {
   double mass;
-}
+} Asteroid;
 
 void initialise_asteroid(Asteroid* asteroid)
 {
-  asteroid->mass = ((rand() * 2) / (rand() % 10)) * 2'260'000'000;
+  asteroid->mass = ((rand() * 2) / (rand() % 10)) * 2260000000;
 }
 
 void shoot_asteroid(Asteroid* asteroid, int* damage_caused)
@@ -34,18 +34,18 @@ void shoot_asteroid(Asteroid* asteroid, int* damage_caused)
 
   // Asteroid mass = 4'520'000'000 means shock_damage_range = 200;
   
-  fatal_damage_range = (asteroid->mass / 2'260'000'000) * 30;
-  shock_damage_range = (asteroid->mass / 2'260'000'000) * 100;
+  fatal_damage_range = (asteroid->mass / 2260000000) * 30;
+  shock_damage_range = (asteroid->mass / 2260000000) * 100;
 
   if (fatal_damage_range >= distance_from_base_km)
   {
-    damage_caused = -1; // Returns fatal as damage_caused
+    *damage_caused = -1; // Returns fatal as damage_caused
     return;
   }
 
   if (distance_from_base_km > shock_damage_range)
   {
-    damage_caused = 0;
+    *damage_caused = 0;
     return;
   }
 
@@ -54,7 +54,7 @@ void shoot_asteroid(Asteroid* asteroid, int* damage_caused)
   relative_shock_damage_power = shock_damage_range / 200;
   relative_base_distance_from_asteroid = (shock_damage_range / distance_from_base_km) - 1;
 
-  damage_caused = relative_shock_damage_power * (shock_damage_range) * relative_base_distance_from_asteroid;
+  *damage_caused = relative_shock_damage_power * (shock_damage_range) * relative_base_distance_from_asteroid;
 
   return;
 }
